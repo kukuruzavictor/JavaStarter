1b3d848e3c8d79f28f0e69b56acfd463f30854c1
Viktor Kukuruza <kukuruzavictor@gmail.com>
2020-04-09 13:57:52 +0300

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class HelloWorld {
+    public static void main (String[] args){
+        String str1 = "• Проісталювати необхідні пакети програм";
+        String str2 = "• Створити репозиторій на сервері із назвою JavaStarter";
+        String str3 = "• Створити гілку dev";
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+
+    }
+}
+
