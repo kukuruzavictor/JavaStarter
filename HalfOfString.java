2a4c7d89c13e02f5c0d219b04ab2e3ec2f81e644
Viktor Kukuruza <kukuruzavictor@gmail.com>
2020-04-20 02:23:25 +0300

    half of string

--- HalfOfString.java
+++ HalfOfString.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class HalfOfString {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введіть рядок: ");
+        String string = sc.nextLine();
+        System.out.println("Рядок:     "+string);
+        System.out.println("Пів рядка: "+halfString(string));
+
+    }
+
+    public static String halfString(String string){
+        return string.substring(0, string.length()/2);
+    }
 }
