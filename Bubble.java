dfe3eae8c16ee35751cececf4358090187c59e4e
Viktor Kukuruza <kukuruzavictor@gmail.com>
2020-04-26 21:24:59 +0300

    Bubble sorting

--- Bubble.java
+++ Bubble.java
@@ -1,35 +1,46 @@
 package com.company;
 
 public class Bubble {
-    public static void main (String[] args) {
-        int [] number = {1,5,567,34,98,34,58,543,67,45,345,689,24,578,13,67,89};
-        boolean isSorted=false;
-        int k=1; // value to decrease the increment of every new inner cycle of bubble method
 
-        System.out.print("          масив: ");
-        for (int item : number) {
-            System.out.print(item + " ");
-        }
+    public static void main (String [] args){
+        int [] array = arrayGenerator();
+        System.out.print("        Generated array: | ");
+        printArray(array);
+        System.out.print("Sorted by BUBBLE method: | ");
+        printArray(bubble(array));
+    }
 
-        //bubble method
-        while (!isSorted) {
+    public static int [] bubble (int [] array){
+        boolean isSorted = false;
+        int k = 1; // value to decrease the increment of every new inner cycle of bubble method
+        while (!isSorted){
             isSorted=true;
-            for (int i=0; i<number.length-k; i++) {
-                if (number[i]>number[i+1]) {
-                    // changing of value assertions without additional 3rd variable
-                    number[i]+=number[i+1];
-                    number[i+1]= number[i]-number[i+1];
-                    number[i]=number[i]-number[i+1];
+            for (int i=0; i<array.length-k; i++){
+                if (array[i]>array[i+1]){
+                    array[i]+=array[i+1];
+                    array[i+1]=array[i]-array[i+1];
+                    array[i]=array[i]-array[i+1];
                     isSorted=false;
                 }
             }
             k+=1;
         }
+        return array;
+    }
 
-        System.out.print("\nметод бульбашки: ");
-        for (int value : number) {
-            System.out.print(value + " ");
+    public static int[] arrayGenerator(){
+        int arrayLength = (int) (5 + (Math.random()*(16)));
+        int [] array = new int[arrayLength];
+        for (int i=0; i<array.length; i++){
+            array[i] = (int) (Math.random()*(1000));
         }
+        return array;
+    }
 
+    public static void printArray(int[] array){
+        for (int value: array) {
+            System.out.print(value+" ");
+        }
+        System.out.println();
     }
 }
